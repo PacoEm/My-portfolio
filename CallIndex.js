bdae86573519d90813020247354e5f1c6f27ccbd
Pascal <pascalzic@outlook.fr>
2022-12-01 18:37:29 +0100

    Adding of the dev page, fixing resize problems

--- CallIndex.js
+++ CallIndex.js
@@ -1,12 +1,16 @@
 // the script manage the appearance of each élément
 // of the index page
 function CallIndex() {
+  const head = document.querySelector("header");
   const titleUp = document.getElementById("title_up");
   const titleDown = document.getElementById("title_down");
 
   const linksUp = document.querySelectorAll(".linkup");
   const linksDown = document.querySelectorAll(".linkdown");
   const separation = document.getElementById("separations_container");
+
+  const dev = document.getElementById("dev");
+  const devsect = document.getElementById("dev_section_container");
 
   // title opacity time transition
   const TITLE_TIME = 3;
@@ -38,6 +42,30 @@ function CallIndex() {
     separation.style.transitionProperty = "opacity";
     separation.style.opacity = "1";
   });
+
+  dev.addEventListener("click", () => {
+    head.style.transform = "translateX(100vw)";
+    head.style.transition = "2s ease";
+    head.style.transitionProperty = "translateX()";
+
+    devsect.style.transform = "translateX(0px)";
+    devsect.style.transition = "2s ease";
+    devsect.style.transitionProperty = "translateX()";
+  });
+
+  window.addEventListener(
+    "resize",
+    function (event) {
+      head.style.transition = "none";
+      devsect.style.transition = "none";
+    },
+    true
+  );
+  window.onresize = ResetTransition();
+  function ResetTransition() {
+    head.style.transition = "none";
+    devsect.style.transition = "none";
+  }
 }
 
 CallIndex();
